dc273db7a9b5c26f0f0ead48d9282903b2a0cbd1
zach.rolfness <zach.rolfness@gmail.com>
2019-09-14 18:51:59 -0400

    updated server

--- opencv/server.py
+++ opencv/server.py
@@ -4,32 +4,36 @@ localIP     = "127.0.0.1"
 localPort   = 6150
 bufferSize  = 1024
 
+connection = False
+
 # Create a datagram socket
 SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind to address and ip
 SOCK.bind((localIP, localPort))
 
+SOCK.sendto(("connecting").encode())
+
 # Listen for incoming datagrams
-while True: 
+while True:
     bytesAddressPair = SOCK.recvfrom(bufferSize)
+
     message = bytesAddressPair[0]
 
     translate = "{}".format(message)
 
-    if(translate=='calibrate'):
-        #write the call function to fischers code
-        #bytesToSend = ""
+    if(translate=='hello'):
+        SOCK.sendto(("Connected").encode())
+        connection = True
 
-        # Sending a reply to client
+    elif(translate=='calibrate' and connection):
         SOCK.sendto(("I'm calibrating").encode())
 
-    elif(translate=='sticky'):
+    elif(translate=='sticky' and connection):
         #write the call function to fischers code
         #bytesToSend = ""
-
         # Sending a reply to client
-        SOCK.sendto(("I'm wet").encode())
+        SOCK.sendto(("P180090234144").encode())
 
     
     
