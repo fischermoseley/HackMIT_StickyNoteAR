dcc78f64446aa9549903469204218800bf60228a
dukeeagle <luke.igel@gmail.com>
2019-09-14 17:01:08 -0400

    prelim keyboard press change to server

--- speaker.py
+++ speaker.py
@@ -1,13 +1,15 @@
 import socket
+import keyboard #make sure to pip install keyboard
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6150
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def send_jump_eventI(sock):
+    sock.sendto( ("JUMP!").encode(), (UDP_IP_ADDRESS, UDP_PORT_NO) )
+    print("Jump Action Triggered!")
 
+if __name__ == "__main__": 
 
-sock.sendto( ("JUMP!").encode(), (UDP_IP, UDP_PORT) )
-print("Jump Action Triggered!")
-
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
