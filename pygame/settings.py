51e6daf24586d8ea3b04291e6f5d11c1d97952d5
Tim Gutterman <55318950+tim4tr0n@users.noreply.github.com>
2019-09-15 02:22:08 -0400

    Added Sticky Note Platform Subclasses

---
+++ pygame/settings.py
@@ -0,0 +1,29 @@
+# game options/settings
+TITLE = "Sticky Jump"
+WIDTH = 1920    
+HEIGHT = 1080
+FPS = 60
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
+                 (125, HEIGHT - 350, 100, 20),
+                 (350, 200, 100, 20),
+                 (175, 100, 50, 20)]
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 255) #Cyan
+YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
+PINK = (255, 69, 178)
+ORANGE = (256, 165, 0)
+
